7905741ee0ed0bead966b7d71a38dc2837fc42d3
edwardscoolhackername <edwardwillems.willems10@gmail.com>
2021-03-04 13:36:07 -0500

    I did my best

---
+++ variables.py
@@ -0,0 +1,17 @@
+# Commenting! Print echoes out whatever's in the brackets.
+# to the terminal when the file is run.
+print("This is very exciting, Python script!")
+ 
+ #Variables up top
+
+name = "Edward"
+age = 25
+
+dragons = ["red", "green", "blue", "white", "black"]
+
+print("My name is " + name)
+
+name = input("What is YOUR name?")
+print("My name used to be " + name)
+
+print("back when I was " + str(age))
